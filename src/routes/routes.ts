1403106ed3eb6fc960c06ec5ea6b45e0a9ada087
nickolasBenakis <nickolasbenele@gmail.com>
2020-05-24 11:57:07 +0300

    fix routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -9,10 +9,10 @@ import {
 
 const router = new Router();
 
-router.get("/api/v1/products", getDinosaurs)
-  .get("/api/v1/products/:id", getDinosaur)
-  .post("/api/v1/products", addDinosaur)
-  .put("/api/v1/products/:id", updateDinosaur)
-  .delete("/api/v1/products/:id", deleteDinosaur);
+router.get("/api/v1/dinosaurs", getDinosaurs)
+  .get("/api/v1/dinosaurs/:id", getDinosaur)
+  .post("/api/v1/dinosaurs", addDinosaur)
+  .put("/api/v1/dinosaurs/:id", updateDinosaur)
+  .delete("/api/v1/dinosaurs/:id", deleteDinosaur);
 
 export default router;
