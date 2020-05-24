9fc7b67ce787ab59a7a44b786d3ff6565979fbbc
nickolasBenakis <nickolasbenele@gmail.com>
2020-05-24 13:46:43 +0300

    Seperated model from controller

--- server.ts
+++ server.ts
@@ -1,12 +1,11 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import router from "./src/routes/routes.ts";
-const port = 8000;
 
+const port = 8000;
 const app = new Application();
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
 console.log(`Server running on port ${port}`);
-
 await app.listen({ port });
--- src/controllers/dinosaurs.ts
+++ src/controllers/dinosaurs.ts
@@ -2,42 +2,19 @@ import { v4 } from "https://deno.land/std/uuid/mod.ts";
 import {
   Dinosaur,
 } from "../types/types.ts";
-
-let dinosaurs: Array<Dinosaur> = [
-  {
-    id: "1",
-    name: "Achillobator",
-    era: "Late Cretaceous",
-    area: "Mongolia",
-    diet: "carnivorous",
-  },
-  {
-    id: "2",
-    name: "Agilisaurus",
-    era: "Late Jurassic",
-    area: "China",
-    diet: "herbivorous",
-  },
-  {
-    id: "3",
-    name: "Melanorosaurus",
-    era: "Late Triassic",
-    area: "South Africa",
-    diet: "omnivorous",
-  },
-];
+import { Dinosaurs } from "../models/dinosaurs.ts";
 
 const getDinosaurs = ({ response }: { response: any }) => {
   response.body = {
     success: true,
-    data: dinosaurs,
+    data: Dinosaurs,
   };
 };
 
 const getDinosaur = (
   { params, response }: { params: { id: string }; response: any },
 ) => {
-  const selectedDino: Dinosaur | undefined = dinosaurs.find((dino) =>
+  const selectedDino: Dinosaur | undefined = Dinosaurs.find((dino) =>
     dino.id === params.id
   );
   if (selectedDino) {
@@ -68,7 +45,7 @@ const addDinosaur = async (
     const { value : dinosaurBody } = await request.body();
     const dinosaur: Dinosaur = dinosaurBody;
     dinosaur.id = v4.generate();
-    dinosaurs.push(dinosaur);
+    Dinosaurs.push(dinosaur);
     response.status = 201;
     response.body = {
       success: true,
@@ -80,18 +57,19 @@ const addDinosaur = async (
 const deleteDinosaur = (
   { params, response }: { params: { id: string }; request: any; response: any },
 ) => {
-  const filteredDinosaurs: Array<Dinosaur> = dinosaurs.filter(
+  const filteredDinosaurs: Array<Dinosaur> = Dinosaurs.filter(
     (dinosaur: Dinosaur) => (dinosaur.id !== params.id),
   );
-  if (filteredDinosaurs.length === dinosaurs.length) {
-    response.status(404);
+  if (filteredDinosaurs.length === Dinosaurs.length) {
+    response.status = 404;
     response.body = {
       success: false,
       msg: "Not found",
     };
   } else {
-    dinosaurs = filteredDinosaurs;
-    response.status(200);
+    Dinosaurs.splice(0, Dinosaurs.length);
+    Dinosaurs.push(...filteredDinosaurs);
+    response.status = 200;
     response.body = {
       success: true,
       msg: `Dinosaur with id ${params.id} has been deleted`,
@@ -106,29 +84,33 @@ const updateDinosaur = async (
     response: any;
   },
 ) => {
-  const requestedDinosaur: Dinosaur | undefined = dinosaurs.find(
+  const requestedDinosaur: Dinosaur | undefined = Dinosaurs.find(
     (dinosaur: Dinosaur) => dinosaur.id === params.id,
   );
 
   if (requestedDinosaur) {
     const { value: updatedValue } = await request.body();
-    dinosaurs = dinosaurs.map((dinosaur: Dinosaur) => {
-      if (dinosaur.id === params.id) {
-        return {
-          ...dinosaur,
-          updatedValue,
-        };
-      } else {
-        return dinosaur;
-      }
-    });
-    response.status(200);
+    const updatedDinosaurs = Dinosaurs.map(
+      (dinosaur: Dinosaur) => {
+        if (dinosaur.id === params.id) {
+          return {
+            ...dinosaur,
+            updatedValue,
+          };
+        } else {
+          return dinosaur;
+        }
+      },
+    );
+    Dinosaurs.splice(0, Dinosaurs.length);
+    Dinosaurs.push(...updatedDinosaurs);
+    response.status = 200;
     response.body({
       success: true,
       msg: `Dinosaur id ${params.id} updated`,
     });
   } else {
-    response.status(404);
+    response.status = 404;
     response.body({
       success: false,
       msg: `Not Found`,
